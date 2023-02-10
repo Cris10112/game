6d64371fdc572e9bb1b225950645f89527e1fe52
Cris10112 <113529204+Cris10112@users.noreply.github.com>
2023-02-10 10:20:25 -0500

    Add files via upload

--- pygame/click_game.py
+++ pygame/click_game.py
@@ -12,3 +12,21 @@ def update():
     wizard.left= wizard.left + 2
     if wizard.left> WIDTH:
          wizard.right = 0
+
+def set_wizard_normal():
+    wizard.image = 'character'
+
+
+def set_wizard_clicked():
+    wizard.image = 'character_click'
+    clock.schedule_unique(set_wizard_normal, .20)
+
+
+def on_mouse_down(pos):
+    if wizard.collidepoint(pos):
+        print("Eek!")
+        set_wizard_clicked()
+        sounds.eep.play()
+    else:
+        print("You missed me!")
+
